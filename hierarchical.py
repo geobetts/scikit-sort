76390e1387d9fb61d394fe2ff5ce8ad6c08143e7
Bettsworth <george.bettsworth@ons.gov.uk>
2021-03-19 16:00:22 +0000

    added pandas functionality and proof of tests

--- hierarchical.py
+++ hierarchical.py
@@ -9,6 +9,7 @@ import unittest
 import numpy as np
 import pandas as pd
 from numpy.testing import assert_array_equal
+from pandas.util.testing import assert_series_equal, assert_frame_equal
 from sklearn.datasets import make_classification
 from sklearn.metrics import accuracy_score
 from warnings import warn
@@ -24,22 +25,20 @@ class HierarchicalHotDeck:
         Prepare data for sort.
         Parameters
         ----------
-        X : array-like of shape (n_samples, n_features)
+        X : numpy.array or pandas.DataFrame of shape (n_samples, n_features)
             Training data.
-        y : array-like of shape (n_samples,) or (n_samples, n_outputs)
+        y : numpy.array or pandas.Series of shape (n_samples,)
             Target values.
         Returns
         -------
         self : object
         """
 
-        variables = [X, y]
-        variable_names = ['X', 'y']
-        types = [np.ndarray] * 2
+        if not isinstance(X, np.ndarray) and not isinstance(X, pd.DataFrame):
+            raise TypeError(f"The training data X needs to be a numpy array or pandas data frame")
 
-        for variable, name, variable_type in zip(variables, variable_names, types):
-            if not isinstance(variable, variable_type):
-                raise TypeError(f"The variable {name} needs to be a {variable_type}")
+        if not isinstance(y, np.ndarray) and not isinstance(y, pd.Series):
+            raise TypeError(f"The targets y need to be a numpy array or pandas series")
 
         if X.shape[1] > 52:
             raise ValueError(f'There are {X.shape[1]} columns. The maximum number of columns is 52')
@@ -48,16 +47,18 @@ class HierarchicalHotDeck:
             warn('It is not recommended to use HierarchicalHotDeck with more than 5 features. '
                  'Consider selecting the most predictive features or using dimension reduction techniques')
 
-        targets = pd.DataFrame(y.transpose())
-        features = pd.DataFrame(X)
+        targets = pd.Series(y.transpose()) if isinstance(y, np.ndarray) else y
+        features = pd.DataFrame(X) if isinstance(X, np.ndarray) else X
+
+        targets.name = 'a' if targets.name is None else targets.name
+        self.targets_name = targets.name
 
         self.data = pd.merge(targets, features, right_index=True, left_index=True)
 
-        alphabet_list = list(string.ascii_lowercase) + list(string.ascii_uppercase)
-
-        self.cols = itertools.islice(alphabet_list, len(list(self.data.columns)))
-
-        self.data.columns = self.cols
+        if isinstance(X, np.ndarray):
+            alphabet_list = list(string.ascii_lowercase) + list(string.ascii_uppercase)
+            cols = itertools.islice(alphabet_list, len(list(self.data.columns)))
+            self.data.columns = cols
 
         return self
 
@@ -66,44 +67,58 @@ class HierarchicalHotDeck:
         Predict on test vectors X.
         Parameters
         ----------
-        X : array-like of shape (n_samples, n_features)
+        X : numpy.array or pandas.DataFrame of shape (n_samples, n_features)
             Test data.
         Returns
         -------
-        output : array-like of shape (n_samples,) or (n_samples, n_outputs)
-            Predicted target values for X.
+        output : numpy.array or pandas.Series of shape (n_samples,)
+            Predicted target values for X. Pandas / numpy usage will depend on the input.
         """
 
-        if not isinstance(X, np.ndarray):
-            raise TypeError(f"The variable X needs to be an ndarray")
+        if not isinstance(X, np.ndarray) and not isinstance(X, pd.DataFrame):
+            raise TypeError(f"The variable X needs to be a numpy array or pandas data frame")
 
-        features_cols = [x for x in self.data.columns if x != 'a']
+        features_cols = [x for x in self.data.columns if x != self.targets_name]
 
-        features = pd.DataFrame(X, columns=features_cols)
+        features = pd.DataFrame(X, columns=features_cols) if isinstance(X, np.ndarray) else X
 
-        features['a'] = np.nan
+        features[self.targets_name] = np.nan
 
         df = features.append(self.data, ignore_index=True, sort=True)
         df = df.reset_index(drop=True)
 
-        index = df['a'].index[df['a'].apply(np.isnan)]
+        index = df[self.targets_name].index[df[self.targets_name].apply(np.isnan)]
 
         df = df.sort_values(features_cols)
-        df['a'] = df['a'].ffill()
-        df['a'] = df['a'].bfill()
+        df[self.targets_name] = df[self.targets_name].ffill()
+        df[self.targets_name] = df[self.targets_name].bfill()
 
-        predictions = df.loc[list(index), 'a']
+        predictions = df.loc[list(index), self.targets_name]
 
-        output = predictions.to_numpy()
+        output = predictions.to_numpy() if isinstance(X, np.ndarray) else predictions
         # if no numbers after the decimal place then return integer output
         output = output.astype(int) if np.isclose(output, np.round(output)).all() else output
 
         return output
 
 
-class TestHierarchicalHotDeck(unittest.TestCase):
+class TestHierarchicalHotDeckNumpy(unittest.TestCase):
     """
     Regression and classification work the same.
+
+    Proof of test:
+    train, test and train targets combined and sorted:
+    | 0 | 1 | blank |
+    | 1 | 6 | 1     |
+    | 2 | 3 | 1     |
+    | 3 | 4 | 0     |
+    | 4 | 5 | 1     |
+    | 4 | 6 | 0     |
+    | 5 | 7 | blank |
+    | 6 | 2 | blank |
+
+    back fill means first blank is 1, forward fill means last two blanks are 0.
+    hence [1, 0, 0]
     """
 
     def test_hierarchical_hotdeck(self):
@@ -120,6 +135,49 @@ class TestHierarchicalHotDeck(unittest.TestCase):
 
         assert_array_equal(x=output, y=expected_output)
         self.assertEqual(accuracy_score(output, test_targets), 1)
+
+
+class TestHierarchicalHotDeckPandas(unittest.TestCase):
+    """
+    Regression and classification work the same.
+
+    Proof of test:
+
+    train, test and train targets combined and sorted:
+    | 'a' | 2 | 1     |
+    | 'a' | 4 | 1     |
+    | 'a' | 5 | blank |
+    | 'a' | 6 | blank |
+    | 'b' | 0 | blank |
+    | 'b' | 1 | 1     |
+    | 'b' | 3 | 0     |
+    | 'b' | 4 | 0     |
+
+     forward fill means [1, 1, 1]
+    """
+
+    def test_hierarchical_hotdeck(self):
+        train = pd.DataFrame({'a': ['a', 'b', 'a', 'b', 'b'],
+                              'b': [2, 1, 4, 3, 4]})
+
+        train_targets = pd.Series([1, 1, 1, 0, 0])
+        train_targets.name = 'f'
+
+        test = pd.DataFrame({'a': ['a', 'b', 'a'],
+                             'b': [6, 0, 5]})
+
+        test_targets = pd.Series([1, 1, 1])
+        test_targets.name = 'f'
+
+        model = HierarchicalHotDeck()
+        fitted_model = model.fit(train, train_targets)
+        output = fitted_model.predict(test)
+
+        expected_output = test_targets
+
+        assert_series_equal(left=output, right=expected_output, check_dtype=False)
+
+        self.assertEqual(accuracy_score(output.to_numpy(), test_targets.to_numpy()), 1)
 
 
 class TestColumnsError(unittest.TestCase):
